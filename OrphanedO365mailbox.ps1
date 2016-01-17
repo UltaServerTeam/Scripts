6564bbba6ccdc1234d815ee631fc0e612d966c76
UltaServerTeam <jalmli@ulta.com>
2016-01-17 16:03:43 -0600

    initial commit

---
+++ OrphanedO365mailbox.ps1
@@ -0,0 +1,6 @@
+﻿#Gets GUID of O365 User
+$RGuid = Get-CloudRemoteMailbox <alias> | Format-List ExchangeGUID 
+
+#Create Remote Contact On Premise
+Enalbe-remotemailbox <alias> -remoteroutingaddress "alias@ultainc.mail.onmicrosoft.com"
+
