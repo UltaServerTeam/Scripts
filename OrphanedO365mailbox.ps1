f2c8e5ce2e38a33ee24f989575f8fd5885d2a439
UltaServerTeam <jalmli@ulta.com>
2016-01-21 14:27:25 -0600

    0365

--- OrphanedO365mailbox.ps1
+++ OrphanedO365mailbox.ps1
@@ -1,6 +1,44 @@
-﻿#Gets GUID of O365 User
-$RGuid = Get-CloudRemoteMailbox <alias> | Format-List ExchangeGUID 
+﻿#Alias Paramater
+param(
+[string]$alias
+)
+
+
+#Please establish an O365 connection before running the script
+Write-Host "Please establish an O365 connection if you have not done so" -BackgroundColor DarkBlue
+Start-Sleep 1
+
+#Establish On-Premise Exchange Connection
+# Connect to On Premises Exchange
+$OnPremiseSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://server.domain.lcl/powershell/ -Authentication Kerberos
+Import-PSSession $OnPremiseSession
+Write-Host "on prem session connected" -BackgroundColor darkblue
+
+#Gets GUID of O365 User
+$RGuid = Get-CloudRemoteMailbox $alias | Format-List ExchangeGUID 
+
+#List GUID of O365 User
+Write-Host "$RGuid" -BackgroundColor DarkRed
+Start-Sleep 2
 
 #Create Remote Contact On Premise
-Enalbe-remotemailbox <alias> -remoteroutingaddress "alias@ultainc.mail.onmicrosoft.com"
+Enable-remotemailbox $alias -remoteroutingaddress "$alias@domain.mail.onmicrosoft.com"
+Write-Host "$alias@domain.mail.onmicrosoft.com has been created"
+
+#Gets GUID of Newly created on Premis user
+$LGuid = Get-RemoteMailbox $alias | Format-List ExchangeGuid
+
+#Lists Guid of Newly created on Premis user
+Write-Host "$LGuid" -BackgroundColor DarkRed
+Start-Sleep 2
+
+#Sets onPremise Guid to match O365 Guid
+Set-RemoteMailbox <alias> -ExchangeGuid $RGuid 
+
+
+
+
+
+
+
 
