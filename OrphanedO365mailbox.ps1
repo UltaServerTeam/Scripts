6f40801de2e09bc63cd3768f7238cf0bae372855
UltaServerTeam <jalmli@ulta.com>
2016-01-21 15:38:28 -0600

    O365 Changes

--- OrphanedO365mailbox.ps1
+++ OrphanedO365mailbox.ps1
@@ -21,21 +21,30 @@ $RGuid = Get-CloudRemoteMailbox $alias | Format-List ExchangeGUID
 Write-Host "$RGuid" -BackgroundColor DarkRed
 Start-Sleep 2
 
-#Create Remote Contact On Premise
+#Create Remote Contact onPremise
 Enable-remotemailbox $alias -remoteroutingaddress "$alias@domain.mail.onmicrosoft.com"
 Write-Host "$alias@domain.mail.onmicrosoft.com has been created"
 
-#Gets GUID of Newly created on Premis user
+#Gets GUID of Newly created onPremise user
 $LGuid = Get-RemoteMailbox $alias | Format-List ExchangeGuid
 
-#Lists Guid of Newly created on Premis user
+#Lists Guid of Newly created onPremis user
 Write-Host "$LGuid" -BackgroundColor DarkRed
 Start-Sleep 2
 
 #Sets onPremise Guid to match O365 Guid
+Write-Host "Setting GUID on New Remote Contact"
 Set-RemoteMailbox $alias -ExchangeGuid $RGuid 
 
 
+#Verify GUIDS onPremise and O365
+
+$RGuid = Get-RemoteMailbox $alias | Format-List ExchangeGuid
+$LGuid = Get-CloudRemoteMailbox $alias | Format-List ExchangeGUID
+Write-Host "Local GUID is $LGuid" -BackgroundColor DarkRed
+Write-Host "Remote GUID is $RGuid" -BackgroundColor DarkRed
+
+Write-Host "GUIDS should match"
 
 
 
