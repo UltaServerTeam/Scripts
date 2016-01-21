cb1b7fad574761266a21b0c07cdc4053b13dabf8
UltaServerTeam <jalmli@ulta.com>
2016-01-21 15:03:14 -0600

    0365

--- OrphanedO365mailbox.ps1
+++ OrphanedO365mailbox.ps1
@@ -9,7 +9,7 @@ Write-Host "Please establish an O365 connection if you have not done so" -Backgr
 Start-Sleep 1
 
 #Establish On-Premise Exchange Connection
-# Connect to On Premises Exchange
+#Connect to On Premises Exchange
 $OnPremiseSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://server.domain.lcl/powershell/ -Authentication Kerberos
 Import-PSSession $OnPremiseSession
 Write-Host "on prem session connected" -BackgroundColor darkblue
@@ -33,7 +33,7 @@ Write-Host "$LGuid" -BackgroundColor DarkRed
 Start-Sleep 2
 
 #Sets onPremise Guid to match O365 Guid
-Set-RemoteMailbox <alias> -ExchangeGuid $RGuid 
+Set-RemoteMailbox $alias -ExchangeGuid $RGuid 
 
 
 
