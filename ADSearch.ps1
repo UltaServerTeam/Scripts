3855ec196ff956ec5bde6e704e0af7a5affd31f7
UltaServerTeam <jalmli@ulta.com>
2016-01-22 08:45:51 -0600

    removed domain

--- ADSearch.ps1
+++ ADSearch.ps1
@@ -1,11 +1,11 @@
 ﻿# Connect to On Premises Exchange
-$OnPremiseSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://b1-pexchch01.ultainc.lcl/powershell/ -Authentication Kerberos
+$OnPremiseSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://server.domanin.com/powershell/ -Authentication Kerberos
 Import-PSSession $OnPremiseSession
 Write-Host "on prem session connected" -BackgroundColor darkblue
 
 
 
-$Users = Get-ADUser -SearchBase "OU=GM's,OU=Field Users,OU=Users,OU=Corporate,OU=ULTA-Managed,DC=ultainc,DC=lcl" -Filter * -Properties displayname,department,sAMAccountName | where {$_.displayname -like '*, gm*' -and $_.department -eq $null} | select displayname,department,samaccountname #| Export-csv C:\Scripts\output\adoutput_GM.csv
+$Users = Get-ADUser -SearchBase "OU=XX,OU=XX Users,OU=Users,OU=XX,OU=XX,DC=XX,DC=XX" -Filter * -Properties displayname,department,sAMAccountName | where {$_.displayname -like '*, gm*' -and $_.department -eq $null} | select displayname,department,samaccountname #| Export-csv C:\Scripts\output\adoutput_GM.csv
 
 foreach ($User in $users)
 {
