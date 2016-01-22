2f8add42c6b032d38e8da5147be37bc345349703
UltaServerTeam <jalmli@ulta.com>
2016-01-22 08:27:39 -0600

    AD Account Creation

---
+++ ADAccountcreation.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module activedirectory
+
+
+$users = Import-csv C:\Scripts\input\users.csv
+
+foreach ($user in $users) 
+{
+
+$displayname = $user.Firstname + " " + $user.Lastname
+$firstname = $user.Firstname
+$lastname = $user.Lastname
+$SAM = $user.SAM
+$UPN = $user.Sam + "@" + $user.Maildomain
+$Description = $user.Description
+$OU = $user.OU
+$expiration = $user.expires
+$Password = $User.password
+New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -UserPrincipalName $UPN -GivenName $UserFirstname -Surname $UserLastname -Description $Description -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -Path "$OU" -ChangePasswordAtLogon $false –PasswordNeverExpires $true -AccountExpirationDate $expiration -server ultainc.lcl          
+}
